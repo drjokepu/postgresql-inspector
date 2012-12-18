a9aa33d8e34c08e6230aa9810bdb55a26b7230eb
Tamas Czinege <tomi.czinege@gmail.com>
2012-12-18 18:10:52 +0000

    some work on loading tables

--- PostgreSQL Inspector/PostgreSQL Inspector/PGRelation.m
+++ PostgreSQL Inspector/PostgreSQL Inspector/PGRelation.m
@@ -21,24 +21,65 @@
 {
     @autoreleasepool
     {
-        NSString *commandText = [[NSString alloc] initWithFormat:
-                                 @"select c.relname, c.relnamespace, c.relowner, c.reltuples, c.relkind, n.nspname from pg_catalog.pg_class c inner join pg_catalog.pg_namespace n on n.oid = c.relnamespace where c.oid = %li", self.oid];
+        static const NSString *commandText =
+        @"select c.relname, "
+        "       c.relnamespace, "
+        "       c.relowner, "
+        "       c.reltuples, "
+        "       c.relkind, "
+        "       n.nspname "
+        "  from pg_catalog.pg_class c "
+        "       inner join pg_catalog.pg_namespace n "
+        "               on n.oid = c.relnamespace "
+        " where c.oid = %li;"
+        "select a.attrelid, "
+        "       a.attname, "
+        "       a.atttypid, "
+        "       pg_catalog.format_type(a.atttypid, a.atttypmod), "
+        "       a.attlen, "
+        "       a.atttypmod, "
+        "       a.attnum, "
+        "       a.attndims, "
+        "       a.attnotnull, "
+        "       d.adsrc "
+        "  from pg_catalog.pg_attribute a "
+        "       left outer join pg_catalog.pg_attrdef d "
+        "                    on ( d.adrelid = a.attrelid "
+        "                         and d.adnum = a.attnum ) "
+        " where a.attrelid = $1 "
+        "       and a.attnum > 0 "
+        "       and a.attisdropped = false "
+        " order by a.attnum;";
+
+        
+        NSString *queryText = [[NSString alloc] initWithFormat:(NSString*)commandText, self.oid];
     
         PGCommand *command = [[PGCommand alloc] init];
         command.connection = connection;
-        command.commandText = commandText;
+        command.commandText = queryText;
         
         [command execAsyncWithCallback:^(PGResult *result) {
-            self.name = (NSString*)result.rows[0][0];
-            self.namespace = [(NSNumber*)result.rows[0][1] integerValue];
-            self.owner = [(NSNumber*)result.rows[0][2] unsignedIntegerValue];
-            self.kind = [(NSNumber*)result.rows[0][4] charValue];
-            self.schemaName = (NSString*)result.rows[0][5];
-            self.columns = [[NSMutableArray alloc] initWithArray:[PGRelationColumn loadColumnsInRelation:self.oid fromCatalog:connection]];
-            
+            switch (result.index)
+            {
+                case 0:
+                    [self loadDetailsFromResult:result];
+                    break;
+            }
+        } noMoreResultsCallback:^{
             if (asyncCallback != nil) asyncCallback();
+        } errorCallback:^(PGError *error) {
+            
         }];
     }
+}
+
+-(void)loadDetailsFromResult:(PGResult*)result
+{
+    self.name = (NSString*)result.rows[0][0];
+    self.namespace = [(NSNumber*)result.rows[0][1] integerValue];
+    self.owner = [(NSNumber*)result.rows[0][2] unsignedIntegerValue];
+    self.kind = [(NSNumber*)result.rows[0][4] charValue];
+    self.schemaName = (NSString*)result.rows[0][5];
 }
 
 -(NSString *)description
